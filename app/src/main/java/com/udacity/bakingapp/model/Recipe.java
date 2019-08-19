2f8bc0747fb1271b5c39b28df4513926e368c9c4
Armando <prieto.arm@gmail.com>
2019-08-18 17:13:52 -0700

    model classes created

---
+++ app/src/main/java/com/udacity/bakingapp/model/Recipe.java
@@ -0,0 +1,70 @@
+package com.udacity.bakingapp.model;
+
+import java.util.List;
+
+public class Recipe {
+
+    private int id;
+    private String name;
+    private List<Ingredient> ingredientsList;
+    private List<Step> stepsList;
+    private int servings;
+    private String image;
+
+    public Recipe(int id, String name, List<Ingredient> ingredientsList, List<Step> stepsList, int servings, String image) {
+        this.id = id;
+        this.name = name;
+        this.ingredientsList = ingredientsList;
+        this.stepsList = stepsList;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredientsList() {
+        return ingredientsList;
+    }
+
+    public void setIngredientsList(List<Ingredient> ingredientsList) {
+        this.ingredientsList = ingredientsList;
+    }
+
+    public List<Step> getStepsList() {
+        return stepsList;
+    }
+
+    public void setStepsList(List<Step> stepsList) {
+        this.stepsList = stepsList;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
