5f659df5c8bf2998b705dedc94d46d05428703ca
Armando <prieto.arm@gmail.com>
2019-08-19 22:49:16 -0700

    change made to fragment to work on two panel mode using view model and live data

--- app/src/main/java/com/udacity/bakingapp/ItemDetailFragment.java
+++ app/src/main/java/com/udacity/bakingapp/ItemDetailFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +16,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.udacity.bakingapp.dummy.DummyContent;
+import com.udacity.bakingapp.model.Recipe;
+import com.udacity.bakingapp.viewModel.BakingViewModel;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -26,12 +31,9 @@ public class ItemDetailFragment extends Fragment {
      * represents.
      */
     public static final String ARG_ITEM_ID = "item_id";
-
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
-
+    private Recipe mItem;
+    private TextView mNameTextView;
+    private CollapsingToolbarLayout appBarLayout;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -42,31 +44,34 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Activity activity = this.getActivity();
+        appBarLayout = activity.findViewById(R.id.toolbar_layout);
 
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
-
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
-        }
-
+        mNameTextView = rootView.findViewById(R.id.item_detail);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            int argId = Integer.valueOf(getArguments().getString(ARG_ITEM_ID));
+            BakingViewModel model = ViewModelProviders.of(this).get(BakingViewModel.class);
+            model.getRecipes().observe(this, recipes -> {
+                recipes.stream().filter(recipe -> recipe.getId() == argId)
+                        .findFirst()
+                        .ifPresent(recipe -> mItem = recipe);
+                if (appBarLayout != null) {
+                    appBarLayout.setTitle(mItem.getName());
+                }
+                mNameTextView.setText(mItem.getName());
+            });
+        }
     }
 }
